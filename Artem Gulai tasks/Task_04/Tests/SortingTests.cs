6b26b40f839c97ec5599175df9c5fc7c83486ffc
Gulai Artem <gulai.artem@gmail.com>
2019-12-11 13:39:44 +0400

    Add some more unit tests

--- Artem Gulai tasks/Task_04/Tests/SortingTests.cs
+++ Artem Gulai tasks/Task_04/Tests/SortingTests.cs
@@ -14,12 +14,90 @@ namespace Task_04.Tests
         [Test]
         public static void TestSortInt()
         {
-            int[] arrayToSort = new int[] { 1,4,7,-1,9,54 };
+            var rand = new Random();
+            var numOfItems = 1000;
+            int[] arrayToSort = new int[numOfItems];
+            for (int i = 0; i < arrayToSort.Length; i++)
+            {
+                arrayToSort[i] = rand.Next(-numOfItems * 10,numOfItems * 10 + 1);
+            }
+
+            // Reference array, sorted using LINQ
+            int[] sortedArray = (int[])arrayToSort.Clone();
+
             CustomSort sortingUnit = new CustomSort();
             sortingUnit.SortArray(arrayToSort,(x,y) => x < y);
-            Assert.AreEqual(arrayToSort,new int[] { -1,1,4,7,9,54 });
+            Assert.AreEqual(arrayToSort,sortedArray.OrderBy(a => a));
             sortingUnit.SortArray(arrayToSort,(x,y) => x > y);
-            Assert.AreEqual(arrayToSort,new int[] { 54,9,7,4,1,-1 });
+            Assert.AreEqual(arrayToSort,sortedArray.OrderByDescending(a => a));
+        }
+
+        [Test]
+        public static void TestSortDouble()
+        {
+            var rand = new Random();
+            var numOfItems = 1000;
+            double[] arrayToSort = new double[numOfItems];
+            for (int i = 0; i < arrayToSort.Length; i++)
+            {
+                arrayToSort[i] = rand.NextDouble() * numOfItems * 10 - numOfItems * 20;
+            }
+
+            // Reference array, sorted using LINQ
+            double[] sortedArray = (double[])arrayToSort.Clone();
+
+            CustomSort sortingUnit = new CustomSort();
+            sortingUnit.SortArray(arrayToSort,(x,y) => x < y);
+            Assert.AreEqual(arrayToSort,sortedArray.OrderBy(a => a));
+            sortingUnit.SortArray(arrayToSort,(x,y) => x > y);
+            Assert.AreEqual(arrayToSort,sortedArray.OrderByDescending(a => a));
+        }
+
+        [Test]
+        public static void TestSortString()
+        {
+            var arrayToSort = new string[]
+            {
+                "abc", "a", "dsfs", "mnsw", "acb", "rcs", "saldkas"
+            };
+
+            var sortedArrayAsc = new string[]
+            {
+                "a", "abc", "acb", "rcs", "dsfs", "mnsw", "saldkas"
+            };
+
+            var sortedArrayDesc = new string[]
+            {
+                "saldkas", "mnsw", "dsfs", "rcs", "acb", "abc", "a"
+            };
+
+            // function for sorting strings by lengths by ascending order,
+            // then by alphabetical order
+            Func<string,string,bool> comparator = new Func<string,string,bool>((a,b) =>
+            {
+                if (a.Length != b.Length)
+                    return a.Length < b.Length;
+
+                for (int i = 0; i < a.Length; i++)
+                    if (a[i] != b[i])
+                        return a[i] < b[i];
+
+                return true;
+            });
+
+            // function for sorting strings by lengths by descending order, 
+            // then by reversed alphabetical order
+            Func<string,string,bool> antiComparator = new Func<string,string,bool>((a,b) =>
+            {
+                return !comparator.Invoke(a,b);
+            });
+
+            CustomSort sortingUnit = new CustomSort();
+
+            sortingUnit.SortArray(arrayToSort,comparator);
+            Assert.AreEqual(sortedArrayAsc,arrayToSort);
+            sortingUnit.SortArray(arrayToSort,antiComparator);
+            Assert.AreEqual(sortedArrayDesc,arrayToSort);
         }
     }
 }
